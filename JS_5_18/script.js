1ae440340e6c34fdf51505ab5bfeb0cfe59b48be
David <79692191+David-lab966@users.noreply.github.com>
2026-05-18 11:17:50 +0800

    Update script.js

--- JS_5_18/script.js
+++ JS_5_18/script.js
@@ -9,3 +9,19 @@ function countup(number) {
   }
 }
 console.log(countup(5));
+
+//countdown
+const countdown = (num) => {
+  let returnArray = [];
+  if(num < 1){
+    return [];
+  }
+  else{
+    returnArray.push(num);
+
+    returnArray = returnArray.concat(countdown(num-1));
+
+    return returnArray;
+  }
+}
+console.log(countdown(2));
